e5d562bdd6f89445332c93990e4f846527202d57
Alex Chi <iskyzh@gmail.com>
2020-08-13 21:05:54 +0800

    lint

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "vue-popperjs/dist/vue-popper.css";
 import "vue-github-buttons/dist/vue-github-buttons.css"; // Stylesheet
 import VueGitHubButtons from "vue-github-buttons";
 Vue.use(VueGitHubButtons);
-import "vue-popperjs/dist/vue-popper.css";
 
 Vue.config.productionTip = false;
 
